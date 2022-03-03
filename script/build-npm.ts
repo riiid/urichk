0d4a9f768d3734de7eb95ba743a840adc2515153
Yongwook Choi <yongwook.choi@riiid.co>
2022-03-03 18:37:04 +0900

    Add mapping for moo → 0.0.3 release (#11)

--- script/build-npm.ts
+++ script/build-npm.ts
@@ -29,6 +29,12 @@ await build({
       url: "https://github.com/riiid/urichk/issues",
     },
   },
+  mappings: {
+    "https://deno.land/x/moo@0.5.1-deno.2/mod.ts": {
+      name: "moo",
+      version: "0.5.1",
+    },
+  },
   declaration: true,
   typeCheck: false,
   test: false,

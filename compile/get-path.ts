f47e8a30f18930caa11bc7bcf0e1b2be1753d5fc
Yongwook Choi <hyperflow@kakao.com>
2022-03-03 13:40:21 +0900

    feat(get-path): Implement `compile/get-path.ts` (#8)

---
+++ compile/get-path.ts
@@ -0,0 +1,44 @@
+import * as ast from "../ast.ts";
+import { kebabToPascal } from "../case.ts";
+import {
+  getSearchParamsTypeCode,
+  getTailRuleForm,
+  urichkPathToString,
+} from "./nextjs-navigation-hook.ts";
+
+export interface CompileConfig {
+  name: string; // kebab-case
+}
+
+export function compile(
+  schema: ast.Urichk,
+  config?: Partial<CompileConfig>,
+): string {
+  const { name = "" } = config ?? {};
+  const defaultFunctionName = `get${kebabToPascal(name)}Path`;
+  return `
+import {encode} from 'querystring';
+
+export interface PathParamsTable {${
+    schema.map((rule) => {
+      const path = JSON.stringify(urichkPathToString(rule.head.path || []));
+      const tailRuleForm = getTailRuleForm(rule.tail);
+      const searchParamsType = getSearchParamsTypeCode(tailRuleForm);
+      return `
+        ${rule.comment ?? ""}
+        [${path}]: ${searchParamsType};
+      `;
+    }).join("\n")
+  }
+}
+
+export default function ${defaultFunctionName}<Path extends keyof PathParamsTable>(path: Path, searchParams?: PathParamsTable[Path]): string {
+  const query = (
+      searchParams ?
+      '?' + encode(searchParams) :
+      ''
+    );
+  return path + query;
+}
+  `;
+}
